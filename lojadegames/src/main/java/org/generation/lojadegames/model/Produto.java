9acbde25e9e88924b0a27ec90dcf843c8cdd0078
becadfd <damas.rebeca@outlook.com>
2023-03-21 14:39:44 -0300

    CRUD Loja de Games

---
+++ lojadegames/src/main/java/org/generation/lojadegames/model/Produto.java
@@ -0,0 +1,92 @@
+package org.generation.lojadegames.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O nome do jogo é obrigatório!")
+	private String nomeJogo;
+	
+	@NotBlank(message = "A marca do jogo é obrigagtória!")
+	private String marca;
+	
+	@Digits(integer = 4, fraction = 2)
+	private BigDecimal preco;
+	
+	private String descricao;
+	
+	@NotBlank(message = "O console do jogo é obrigagtório!")
+	private String console;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria; 
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomeJogo() {
+		return nomeJogo;
+	}
+
+	public void setNomeJogo(String nomeJogo) {
+		this.nomeJogo = nomeJogo;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getConsole() {
+		return console;
+	}
+
+	public void setConsole(String console) {
+		this.console = console;
+	}
+	
+	
+
+}
